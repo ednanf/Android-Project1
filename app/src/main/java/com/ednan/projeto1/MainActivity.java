a0c2a4ccb66876acdcccdc890c69867d5d64411b
Ednan Rogério Frizzera Filho <ednanrff@gmail.com>
2023-08-02 14:59:41 -0300

    Projeto finalizado.

--- app/src/main/java/com/ednan/projeto1/MainActivity.java
+++ app/src/main/java/com/ednan/projeto1/MainActivity.java
@@ -1,14 +1,32 @@
 package com.ednan.projeto1;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    // main method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // functions
+    @SuppressLint("SetTextI18n")
+    public void selecionarBotao(View view) {
+
+        int numero = new Random().nextInt(11);
+
+        TextView texto = findViewById(R.id.textoResultado); // creates a representation of the object with a specified id
+        texto.setText("O número sorteado foi: " + numero);
+
+    }
+
 }
